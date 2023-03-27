3ec97e15ee10b7d47ca5eeee18e0b0decda68c33
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2023-03-28 00:05:56 +0200

    another mixin refactor

--- graphenix/mixins/mixin_model_base.py
+++ graphenix/mixins/mixin_model_base.py
@@ -6,13 +6,30 @@ T = TypeVar('T', bound='ModelBaseMixin')
 class ModelBaseMixin(metaclass=abc.ABCMeta):
 
     _id = -1
-    
-    @abc.abstractmethod
-    def get(self, id: int) -> T:
-        ...
+
+    # cached values
+    _cache_init: bool = False
+    _model_fields: tuple = tuple()
+    _total_size: int = 0
+    _field_sizes: dict[str, int] = {}
+    _field_types: dict[str, type] = {}
+    _field_types_raw: dict[str, int] = {} # for parsing in c++
+    _field_defaults: dict = {} # can't add type hints since there are different types for default (base of field type)
 
     @abc.abstractmethod
     def save(self) -> None:
-        ...
+        raise NotImplementedError
+    
+    @abc.abstractclassmethod
+    def get(cls, id: int):
+        raise NotImplementedError
+    
+    @abc.abstractclassmethod
+    def _get_fields(cls):
+        raise NotImplementedError
+
+    @abc.abstractclassmethod
+    def _make_cache(cls):
+        raise NotImplementedError
 
     
--- graphenix/model.py
+++ graphenix/model.py
@@ -1,21 +1,13 @@
 import graphenix_engine2
 
+from typing import Type
 from .mixins.mixin_model_base import ModelBaseMixin, T
 from .query import ModelQueryMixin
 from .field import Field, FieldTypeEnum
 
-class Model(ModelQueryMixin, ModelBaseMixin):
+class Model(ModelBaseMixin, ModelQueryMixin):
     __db__ = None
     __lazy_delete__ = False
-
-    # cached values
-    __cache_init: bool = False
-    __model_fields: tuple = tuple()
-    __total_size: int = 0
-    __field_sizes: dict[str, int] = {}
-    __field_types: dict[str, type] = {}
-    __field_types_raw: dict[str, int] = {} # for parsing in c++
-    __field_defaults: dict = {} # can't add type hints since there are different types for default (base of field type)
 
     def __init__(self, **fields):
         self._id = -1
@@ -24,12 +16,12 @@ class Model(ModelQueryMixin, ModelBaseMixin):
             setattr(self, key, value)
 
     def __str__(self):
-        fields = ['id', *self.__get_fields()]
+        fields = ['id', *self._get_fields()]
         attrs = ', '.join([f"{k}={getattr(self, k)}" for k in fields])
         return f"{self.__name__}({attrs})"
     
     def __setattr__(self, name, value):
-        fields = self.__get_fields()
+        fields = self._get_fields()
         if name not in fields and not name.startswith("_"):
             raise AttributeError(f"Cannot assign member '{name}' for type '{self.__name__}'")
         
@@ -44,8 +36,8 @@ class Model(ModelQueryMixin, ModelBaseMixin):
         return self.id == -1
     
     @classmethod
-    def __make_cache(cls) -> None:
-        if cls.__cache_init:
+    def _make_cache(cls: Type[T]) -> None:
+        if cls._cache_init:
             return
         
         field_sizes = {}
@@ -56,8 +48,8 @@ class Model(ModelQueryMixin, ModelBaseMixin):
 
                 field_sizes[field_name] = field.size
                 actual_type = type(field)
-                cls.__field_types[field_name] = actual_type
-                cls.__field_defaults[field_name] = field.default
+                cls._field_types[field_name] = actual_type
+                cls._field_defaults[field_name] = field.default
 
                 match actual_type:
                     case Field.Int:
@@ -76,31 +68,31 @@ class Model(ModelQueryMixin, ModelBaseMixin):
                         raise AttributeError("Field type is not valid!")
                 
 
-                cls.__field_types_raw[field_name] = raw_type_index
+                cls._field_types_raw[field_name] = raw_type_index
         
-        cls.__field_sizes = field_sizes
-        cls.__total_size = sum(field_sizes.values())
-        cls.__cache_init = True
+        cls._field_sizes = field_sizes
+        cls._total_size = sum(field_sizes.values())
+        cls._cache_init = True
     
     @classmethod
-    def __get_fields(cls):
-        if not cls.__model_fields:
-            cls.__model_fields = tuple(attr for attr, val in cls.__dict__.items() if isinstance(val, Field.BaseType))
+    def _get_fields(cls: Type[T]):
+        if not cls._model_fields:
+            cls._model_fields = tuple(attr for attr, val in cls.__dict__.items() if isinstance(val, Field.BaseType))
 
-        return cls.__model_fields
+        return cls._model_fields
     
     @classmethod
-    def get(cls, record_id):
-        fields = cls.__get_fields()
-        cls.__make_cache()
+    def get(cls: Type[T], record_id):
+        fields = cls._get_fields()
+        cls._make_cache()
 
-        field_sizes = cls.__field_sizes
+        field_sizes = cls._field_sizes
         sizes_as_list = [field_sizes[field] for field in fields]
-        raw_type_as_list = [cls.__field_types_raw[field] for field in fields]
+        raw_type_as_list = [cls._field_types_raw[field] for field in fields]
 
         record = graphenix_engine2.schema_get_record(cls.__db__, cls.__name__, # type: ignore
                                                      record_id, sizes_as_list,
-                                                     raw_type_as_list, cls.__total_size)
+                                                     raw_type_as_list, cls._total_size)
         
         record_as_dict = {field: record[idx] for idx, field in enumerate(fields)}
         instance = cls(**record_as_dict)
@@ -110,11 +102,11 @@ class Model(ModelQueryMixin, ModelBaseMixin):
     def __get_values(self, fields) -> list:
         result: list = []
         for field in fields:
-            match self.__field_types[field]:
+            match self._field_types[field]:
                 case Field.Link:
-                    result.append(getattr(self, '_' + field + '_id', self.__field_defaults[field]))
+                    result.append(getattr(self, '_' + field + '_id', self._field_defaults[field]))
                 case _:
-                    result.append(getattr(self, '_' + field, self.__field_defaults[field]))
+                    result.append(getattr(self, '_' + field, self._field_defaults[field]))
         
         return result
     
@@ -126,7 +118,7 @@ class Model(ModelQueryMixin, ModelBaseMixin):
             lazy = self.__lazy_delete__
 
         graphenix_engine2.schema_delete_record(self.__db__, self.__name__, # type: ignore
-                                               self.id, lazy, self.__total_size)
+                                               self.id, lazy, self._total_size)
         self._id = -1 # set flag to is_new again so you don't update an inactive record
 
     def make(self: T) -> T:
@@ -134,23 +126,23 @@ class Model(ModelQueryMixin, ModelBaseMixin):
         return self
     
     def save(self):
-        fields = self.__get_fields()
-        self.__make_cache()
+        fields = self._get_fields()
+        self._make_cache()
 
-        field_sizes = self.__field_sizes
+        field_sizes = self._field_sizes
         sizes_as_list = [field_sizes[field] for field in fields]
         values_as_list = self.__get_values(fields)
-        raw_type_as_list = [self.__field_types_raw[field] for field in fields]
+        raw_type_as_list = [self._field_types_raw[field] for field in fields]
 
         if self.is_new:
             self._id = graphenix_engine2.schema_add_record(self.__db__, self.__name__, # type: ignore
                                                             values_as_list, sizes_as_list,
-                                                            self.__total_size,
+                                                            self._total_size,
                                                             raw_type_as_list)
         else:
             graphenix_engine2.schema_update_record(self.__db__, self.__name__, self.id,  # type: ignore
                                                    values_as_list, sizes_as_list,
-                                                   self.__total_size,
+                                                   self._total_size,
                                                    raw_type_as_list)
 
 
