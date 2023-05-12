be1e890b6a7691b71af4b59a4be5e532ed82f042
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2023-05-13 00:56:32 +0200

    recursive load & read for bptree

--- graphenix/engine/bptree/bptreeindex.hpp
+++ graphenix/engine/bptree/bptreeindex.hpp
@@ -10,47 +10,57 @@ using namespace std;
 class BPTreeIndex
 {
 public:
-    string ix_file;
+    string ix_filename;
     BPTreeNode *root;
 
     BPTreeIndex(string model_name, string field_name)
     {
         // when stable should use -> get_field_ix_file_name;
-        ix_file = "bpt_ix_" + model_name + "_" + field_name + ".bin";
+        ix_filename = "bpt_ix_" + model_name + "_" + field_name + ".bin";
         root = NULL;
     }
 
     void create()
     {
-        if (filesystem::exists(ix_file))
+        if (filesystem::exists(ix_filename))
             return;
 
-        ofstream ix_file_stream(ix_file, ios::binary | ios::out);
-        ix_file_stream.close();
+        ofstream ix_file(ix_filename, ios::binary | ios::out);
+        ix_file.close();
         root = new BPTreeNode(0);
+        ix_file.close();
     }
 
     void make()
     {
-        fstream ix_file_stream(ix_file, ios::binary | ios::in | ios::out);
-        root->write(ix_file_stream);
+        fstream ix_file(ix_filename, ios::binary | ios::in | ios::out);
+        root->write(ix_file);
+        ix_file.close();
     }
 
     void read()
     {
-        fstream ix_file_stream(ix_file, ios::binary | ios::in | ios::out);
-        root->read(ix_file_stream);
+        fstream ix_file(ix_filename, ios::binary | ios::in | ios::out);
+        root->read(ix_file);
+        ix_file.close();
     }
 
-    void print_index()
+    void load_full_tree()
+    {
+        fstream ix_file(ix_filename, ios::binary | ios::in | ios::out);
+        root->read_recursive(ix_file);
+        ix_file.close();
+    }
+
+    void print_index(bool is_recursive)
     {
         if (root != NULL)
-            root->print();
+            root->print(is_recursive);
     }
 
     void delete_index()
     {
-        if (filesystem::exists(ix_file))
-            filesystem::remove(ix_file);
+        if (filesystem::exists(ix_filename))
+            filesystem::remove(ix_filename);
     }
 };
--- graphenix/engine/bptree/bptreenode.hpp
+++ graphenix/engine/bptree/bptreenode.hpp
@@ -25,6 +25,7 @@ public:
     bool is_leaf;
     vector<int64_t> keys;
     vector<int64_t> children, data;
+    vector<BPTreeNode> actual_children;
     int64_t prev, next;
 
     BPTreeNode(int64_t _offset)
@@ -38,7 +39,7 @@ public:
         ix_file.seekp(offset, std::ios::beg);
         size_t num_keys = keys.size();
         char *buffer = new char[BLOCK_SIZE];
-        memset(buffer, 0, BLOCK_SIZE);
+        memset(buffer, -1, BLOCK_SIZE);
         char *buffer_ptr = buffer;
         memcpy(buffer_ptr, reinterpret_cast<const char *>(&num_keys), IX_SIZE);
         memcpy(buffer_ptr + IX_SIZE, reinterpret_cast<const char *>(&is_leaf), IX_SIZE);
@@ -61,7 +62,7 @@ public:
         }
         else
         {
-            for (const auto &child: children)
+            for (const auto &child : children)
             {
                 memcpy(buffer_ptr, reinterpret_cast<const char *>(&child), IX_SIZE);
                 buffer_ptr += IX_SIZE;
@@ -70,6 +71,25 @@ public:
 
         ix_file.write(buffer, BLOCK_SIZE);
         delete[] buffer;
+    }
+
+    /**
+     * @brief This method is a simple DFS to read the tree into memory
+     * @param ix_file : stream for reading the file (shouldn't be closed while reading)
+     */
+    void read_recursive(fstream &ix_file)
+    {
+        read(ix_file); // read current
+        actual_children.clear();
+        for (const auto &child : children)
+        {
+            if (child != -1)
+            {
+                BPTreeNode temp(child); // create with offset
+                temp.read(ix_file);
+                actual_children.push_back(temp);
+            }
+        }
     }
 
     void read(fstream &ix_file)
@@ -86,27 +106,37 @@ public:
 
         cout << "Read num keys: " << num_keys << endl;
         char *buffer_ptr = buffer + 4 * IX_SIZE;
-        keys.assign(reinterpret_cast<int64_t*>(buffer_ptr), reinterpret_cast<int64_t*>(buffer_ptr + num_keys * IX_SIZE));
+        keys.assign(reinterpret_cast<int64_t *>(buffer_ptr), reinterpret_cast<int64_t *>(buffer_ptr + num_keys * IX_SIZE));
         buffer_ptr += num_keys * IX_SIZE;
         if (is_leaf)
             data.assign(
-                reinterpret_cast<int64_t*>(buffer_ptr), 
-                reinterpret_cast<int64_t*>(buffer_ptr + num_keys * IX_SIZE)
-            );
+                reinterpret_cast<int64_t *>(buffer_ptr),
+                reinterpret_cast<int64_t *>(buffer_ptr + num_keys * IX_SIZE));
         else
             children.assign(
-                reinterpret_cast<int64_t*>(buffer_ptr), 
-                reinterpret_cast<int64_t*>(buffer_ptr + (num_keys + 1) * IX_SIZE)
-            );
+                reinterpret_cast<int64_t *>(buffer_ptr),
+                reinterpret_cast<int64_t *>(buffer_ptr + (num_keys + 1) * IX_SIZE));
 
         delete[] buffer;
     }
 
-    void print()
+    void print(bool is_recursive)
     {
+        cout << "Offset: " << offset << endl;
+        cout << "Children: " << children.size() << endl;
         for (size_t i = 0; i < keys.size(); i++)
         {
-            cout << "pair: " << keys[i] << ", " << data[i] << endl;
+            cout << "Node: " << keys[i];
+            if (is_leaf) {
+                cout << ", " << data[i];
+            }
+            cout << endl;
         }
+
+        if (!is_recursive)
+            return;
+
+        for (auto &child : actual_children)
+            child.print(true);
     }
 };
