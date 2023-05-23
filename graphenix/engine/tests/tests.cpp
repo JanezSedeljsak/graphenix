515c7383ea1a7c46e091c5fd9cde7cdb90b8c7b0
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2023-05-23 23:33:14 +0200

    cpp test fix - pipeline should now succeed

--- graphenix/engine/tests/tests.cpp
+++ graphenix/engine/tests/tests.cpp
@@ -9,7 +9,7 @@ TEST_CASE("Create bptree node")
 {
     BPTreeNode<int64_t> *n = new BPTreeNode<int64_t>(0, 8);
     unique_ptr<BPTreeNode<int64_t>> n_ptr = unique_ptr<BPTreeNode<int64_t>>(n);
-    CHECK(n_ptr->is_leaf == false);
+    CHECK(n_ptr->is_leaf == true);
     double avg;
     BENCHMARK(n_ptr->flush(), 100, avg);
     // todo: write avg to txt file to get results
