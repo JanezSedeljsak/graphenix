35aa25c5ba60608a6a3a4522351dc1f47054ebe8
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2023-07-27 03:52:18 +0200

    add validation to searlizer

--- graphenix/searilize.py
+++ graphenix/searilize.py
@@ -3,6 +3,35 @@ from datetime import datetime
 class ViewSearilizer:
 
     fields = ()
+    required = ()
+
+    @classmethod
+    def validate(cls, data):
+        if isinstance(data, list):
+            return all(cls.validate(row) for row in data)
+
+        if isinstance(data, dict):
+            keys_with_values = set(key for key, val in data.items() if (val or val == 0))
+            diff = set(cls.required).difference(keys_with_values)
+            if diff:
+                return False
+            
+            for field in cls.required:
+                current_value = data[field]
+                if isinstance(current_value, list) or isinstance(current_value, dict):
+                    if not hasattr(cls, field):
+                         raise AttributeError(f'Field - "{field}" should have a searilizer!')
+                    
+                    searilizer = getattr(cls, field)
+                    if not issubclass(searilizer, ViewSearilizer):
+                        raise AttributeError(f'Field - "{field}" is not as a searilizer!')
+
+                    if not searilizer.validate(current_value):
+                        return False
+
+            return True
+
+        return False
 
     @classmethod
     def jsonify(cls, data):
