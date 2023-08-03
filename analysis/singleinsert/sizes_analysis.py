f6a9bcdcbb32f6802a4b5f95843237f134ea141b
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2023-08-03 23:16:59 +0200

    - improved sizes graph - added big and updated diploma pdf

--- analysis/singleinsert/sizes_analysis.py
+++ analysis/singleinsert/sizes_analysis.py
@@ -29,26 +29,27 @@ def main():
 
     fig, ax = plt.subplots(figsize=(16, 12))
 
-    total_size = ax.bar(np.arange(3), mb_indexed_sizes, color=['#B1B04F', '#649A7C', '#1784A9'])
-    index_diff_size = ax.bar(np.arange(3), mb_indexed_sizes - mb_sizes, color=['#EEEC7B', '#92D6B4', '#35BFEC'])
+    total_size = ax.bar(np.arange(3), mb_indexed_sizes, color=['#EEEC7B', '#92D6B4', '#35BFEC'])
+    index_diff_size = ax.bar(np.arange(3), mb_indexed_sizes - mb_sizes, color=['#B1B04F', '#649A7C', '#1784A9'])
 
     
 
     # Set labels and title
     ax.set_xlabel('Podatkovni sistem')
     ax.set_ylabel('Velikost (MB)')
+    ax.set_ylim(0, 120)
     ax.set_title('')
     ax.set_xticks(np.arange(3))
     ax.set_xticklabels(['Graphenix', 'SQLite', 'MySQL'], rotation=0)
     for ix_size, totals in zip(index_diff_size, total_size):
         height = totals.get_height()
         bottom_offset = 0.5
-        ax.text(totals.get_x() + totals.get_width() / 2, bottom_offset, f'{height:.2f} MB', 
+        ax.text(totals.get_x() + totals.get_width() / 2, height + bottom_offset, f'{height:.2f} MB', 
                 ha='center', va='bottom', fontweight='normal')
         
         iheight = ix_size.get_height()
         ax.text(totals.get_x() + totals.get_width() / 2, iheight + bottom_offset, f'{(iheight):.2f} MB', 
-                ha='center', va='bottom', fontweight='normal', color="white")
+                ha='center', va='bottom', fontweight='normal')
 
     plt.savefig(f'./analysis/singleinsert/sizes.png', bbox_inches='tight')
     plt.close()
