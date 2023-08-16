0e8d9bdf28e422907a40da6264030c1553b4f24d
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2023-08-16 16:24:08 +0200

    - dailyplanner example - remove zone - version bump

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
-from pybind11.setup_helpers import Pybind11Extension
-from pybind11 import get_include
-from setuptools.command.build_ext import build_ext
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __engine_name__ = "graphenix_engine2"
 __name__ = "graphenix"
 
@@ -13,7 +10,6 @@ __author_data = {"author": "janezsedeljsak",
 setup(name=__name__,
       version=__version__,
       packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-      install_requires=["pybind11", "graphenix_engine2"],
       description="Graphenix library",
       # exclude_package_data={"": ["*.cpp", "*.h", "*.hpp", "*.pyx", "*.pxd"]},
       **__author_data)
